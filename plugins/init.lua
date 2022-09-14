ab7f3c7f3ad22ab30ee4546c99301fd3f0f69f26
Daniel Thompson <me@danielthompson.dev>
2022-09-14 00:21:22 -0500

    style(example): Removed example comments in plugins

--- plugins/init.lua
+++ plugins/init.lua
@@ -7,11 +7,4 @@ return {
 
   { "folke/lua-dev.nvim" },
 
-  -- We also support a key value style plugin definition similar to NvChad:
-  -- ["ray-x/lsp_signature.nvim"] = {
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
 }

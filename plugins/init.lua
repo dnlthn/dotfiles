30b2c35629ea3a5ec4e9b760a9575de79d8f6bd2
Daniel Thompson <me@danielthompson.dev>
2022-09-14 00:21:28 -0500

    fix(plugins): Disable better-escape.nvim

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,4 +1,5 @@
 return {
+  ["max397574/better-escape.nvim"] = { disable = true },
   -- We also support a key value style plugin definition similar to NvChad:
   -- ["ray-x/lsp_signature.nvim"] = {
   --   event = "BufRead",

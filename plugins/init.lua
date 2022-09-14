f42f8fbd17dd51f175841f4df5baa174bced3795
Daniel Thompson <me@danielthompson.dev>
2022-09-14 00:21:26 -0500

    feat(plugins): Add lsp_signature.nvim

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,5 +1,8 @@
 return {
   ["max397574/better-escape.nvim"] = { disable = true },
+
+  ["ray-x/lsp_signature.nvim"] = require "user.plugins.lsp_signature",
+
   { "folke/lua-dev.nvim" },
   -- We also support a key value style plugin definition similar to NvChad:
   -- ["ray-x/lsp_signature.nvim"] = {

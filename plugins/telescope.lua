2878f5df172b8a82206d2c9d45b60c3402fa3613
Daniel Thompson <me@danielthompson.dev>
2022-09-16 17:16:11 -0500

    fix(telescope): Add closing telescope with "q"

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,6 +1,14 @@
+local actions = require "telescope.actions"
+
 return {
   defaults = {
     selection_caret = " ",
     results_title = false,
+
+    mappings = {
+      n = {
+        ["q"] = actions.close,
+      },
+    },
   },
 }

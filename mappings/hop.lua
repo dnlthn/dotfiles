212b1d8f688db8df846c0f2d03164f62de8c8dde
Daniel Thompson <me@danielthompson.dev>
2022-09-21 14:57:38 -0500

    fix(mappings/hop): Fix "t" mapping to properly go "to" the character

--- mappings/hop.lua
+++ mappings/hop.lua
@@ -2,10 +2,69 @@ return {
   n = {
     ["f"] = { "<cmd>HopChar1<cr>", desc = "Hop to letter" },
     ["F"] = { "<cmd>HopChar1CurrentLine<cr>", desc = "Hop to letter on line" },
+
+    ["t"] = {
+      function()
+        require("hop").hint_char1 {
+          direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+          current_line_only = true,
+          hint_offset = -1,
+        }
+      end,
+      desc = "Hop before letter on line",
+    },
+    ["T"] = {
+      function()
+        require("hop").hint_char1 {
+          hint_offset = -1,
+        }
+      end,
+      desc = "Hop before letter",
+    },
+  },
+
+  o = {
+    ["t"] = {
+      function()
+        require("hop").hint_char1 {
+          direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+          current_line_only = true,
+          hint_offset = -1,
+        }
+      end,
+      desc = "Hop before letter on line",
+    },
+    ["T"] = {
+      function()
+        require("hop").hint_char1 {
+          hint_offset = -1,
+        }
+      end,
+      desc = "Hop before letter",
+    },
   },
 
   v = {
     ["f"] = { "<cmd>HopChar1<cr>", desc = "Hop to letter" },
     ["F"] = { "<cmd>HopChar1CurrentLine<cr>", desc = "Hop to letter on line" },
+
+    ["t"] = {
+      function()
+        require("hop").hint_char1 {
+          direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+          current_line_only = true,
+          hint_offset = -1,
+        }
+      end,
+      desc = "Hop before letter on line",
+    },
+    ["T"] = {
+      function()
+        require("hop").hint_char1 {
+          hint_offset = -1,
+        }
+      end,
+      desc = "Hop before letter",
+    },
   },
 }
